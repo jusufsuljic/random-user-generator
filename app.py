96663f2fce5afe02d140a554d378e679db6e190f
Jusuf Suljic <jusuf.uni@gmail.com>
2022-03-10 23:11:42 +0100

    Serve method fix

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
-def serve():
+def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 api.add_resource(RandomUserApi, '/random-user')
